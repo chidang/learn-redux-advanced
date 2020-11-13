43a7b4bd5ba253dd641ef2a82130f6c11020161a
Dang Chi <chid@jobready.com.au>
2020-11-13 19:43:42 +0700

    Dispatching Actions from within a Component

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,29 @@ const inititalState = {
 }
 
 const reducer = (state = inititalState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    if (action.type === 'ADD_FIVE') {
+        return {
+            counter: state.counter + 5
+        }
+    }
+
+    if (action.type === 'SUBTRACT_FIVE') {
+        return {
+            counter: state.counter - 5
+        }
+    }
     return state;
 };
 
